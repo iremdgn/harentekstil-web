a4204cb0600a55eb8c08ab0cfb1de2d81602ba9c
Ömer Emre Elmalı <omeremreelma@gmail.com>
2022-02-01 13:03:12 +0300

    Update css

---
+++ pages/about/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AboutPage = () => {
+    return (
+        <div>
+            <p> Hakkımızda Sayfası </p>
+        </div>
+    );
+}
+
+export default AboutPage;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import HomePage from './home'
 
 export default function Home() {
