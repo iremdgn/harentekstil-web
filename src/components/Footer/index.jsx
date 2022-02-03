3e11a8db6b0548621479f91618d24d1f86a4aa27
İrem Doğan <65050139+iremdgn@users.noreply.github.com>
2022-02-03 20:00:44 +0300

    footer update

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Container } from 'react-bootstrap'
+import Image from 'next/image'
 
-function Footer () {
+function Footer() {
     return (
-        <div>
-            
+        <div className="footer py-5">
+            <Container>
+                <div>
+                    <h3>
+                        <span className="mx-3">
+                            <Image src="/assets/images/logo.jpg" alt="logo" width="45" height="45" />
+                        </span>
+                        <span>HAREN TEKSTİL</span>
+                    </h3>
+                    <p className="px-3 pt-3">Copyright © 2020 Haren Tekstil | All Rights Reserved | Designed By Redvizor</p>
+                </div>
+            </Container>
         </div>
     );
 }
