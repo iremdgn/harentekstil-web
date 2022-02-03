a623cd1c600eca353fe0e6fe037f33b066ab0aae
İrem Doğan <65050139+iremdgn@users.noreply.github.com>
2022-02-03 12:40:34 +0300

    home page contact

--- src/components/HomePageProduct/index.js
+++ src/components/HomePageProduct/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ProductCard from './productCard'
 import ProductService from './productService';
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col, Button } from 'react-bootstrap'
 
 function HomePageProduct() {
     return (
@@ -18,6 +18,20 @@ function HomePageProduct() {
             </Container>
 
             <ProductService />
+
+            <div className="homepage-contact">
+                <Container className="p-5">
+                    <Row className="p-5">
+                        <Col>
+                            <h4>Bizimle İletişime Geçebilirsiniz</h4>
+                            <p className="pt-2">En son modellerimiz, ürün güncellemelerimiz ve daha fazlası hakkında bilgi almak için bizi arayın.</p>
+                        </Col>
+                        <Col className="d-flex justify-content-center">
+                            <Button className="rounded-0 border-0 my-5 px-5" size="lg" type="submit">Bize Ulaşın</Button>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
 
         </>
     );
