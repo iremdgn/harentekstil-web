7729ddd4790d44123bb4d5295999c61807acc414
İrem Doğan <65050139+iremdgn@users.noreply.github.com>
2022-02-22 16:09:36 +0300

    dropdown menu

--- src/components/Header/menu.jsx
+++ src/components/Header/menu.jsx
@@ -1,28 +1,59 @@
 import React from 'react'
-import { Nav } from 'react-bootstrap'
+import { Nav, NavDropdown } from 'react-bootstrap'
+import Image from 'next/image'
 
 function Menu() {
     return (
         <>
             <Nav className="justify-content-center" >
-                <Nav.Item>
-                    <Nav.Link  href="/home" eventKey="link-1">ANASAYFA</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link  href="/products" eventKey="link-2">ÜRÜNLERİMİZ</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link  href="/about" eventKey="link-3">HAKKIMIZDA</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link  href="/blog" eventKey="link-3">BLOG</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link  href="/contact" eventKey="link-4">İLETİŞİM</Nav.Link>
-                </Nav.Item>
+
+                <Nav.Link href="/home" eventKey="link-1">ANASAYFA</Nav.Link>
+
+                <NavDropdown title="ÜRÜNLERİMİZ" id="basic-nav-dropdown">
+                    <NavDropdown.Item className="dropdown title" href="/products">KATEGORİLER <span></span></NavDropdown.Item>
+                    <NavDropdown.Item className="dropdown subtitle " href="">Kategori 1</NavDropdown.Item>
+                    <NavDropdown.Item className="dropdown subtitle " href="">Kategori 2</NavDropdown.Item>
+                    <NavDropdown.Item className="dropdown subtitle " href="">Kategori 3</NavDropdown.Item>
+                    <NavDropdown.Item className="dropdown subtitle " href="">Kategori 4</NavDropdown.Item>
+                    <NavDropdown.Item className="dropdown subtitle " href="">Kategori 5</NavDropdown.Item>
+                    <NavDropdown.Item className="dropdown subtitle-image pt-4" href="/product">
+                        <Image className="dropdown-image" src="/assets/images/slider3.jpg" alt="slider1" width="180" height="120" />
+                        <p className="img-text pt-2">Ürünleri İncele</p>
+                    </NavDropdown.Item>
+
+                </NavDropdown>
+
+                <NavDropdown title="HAKKIMIZDA" id="basic-nav-dropdown">
+                    <NavDropdown.Item className="dropdown title" href="/about">HAKKIMIZDA <span></span></NavDropdown.Item>
+                    <NavDropdown.Item className="dropdown subtitle " href="/about">Biz Kimiz?</NavDropdown.Item>
+                    <NavDropdown.Item className="dropdown subtitle " href="/about">Vizyon/Misyon</NavDropdown.Item>
+                    <NavDropdown.Item className="dropdown subtitle " href="/about">Hizmetlerimiz</NavDropdown.Item>
+                    <NavDropdown.Item className="dropdown pt-4" href="">
+                        <Image className="dropdown-image" src="/assets/images/slider3.jpg" alt="slider1" width="180" height="120" />
+                        <p className="img-text pt-2">Bizi Tanıyın</p>
+                    </NavDropdown.Item>
+                </NavDropdown>
+
+                <NavDropdown title="BLOG" id="basic-nav-dropdown">
+                    <NavDropdown.Item className="dropdown title" href="/blog">YAZILARIMIZ <span></span></NavDropdown.Item>
+                    <NavDropdown.Item className="dropdown subtitle " href="/blog">Yazı 1</NavDropdown.Item>
+                    <NavDropdown.Item className="dropdown subtitle " href="/blog">Yazı 2</NavDropdown.Item>
+                    <NavDropdown.Item className="dropdown subtitle " href="/blog">Yazı 3</NavDropdown.Item>
+                    <NavDropdown.Item className="dropdown pt-4" href="">
+                        <Image className="dropdown-image" src="/assets/images/slider3.jpg" alt="slider1" width="180" height="120" />
+                        <p className="img-text pt-2">Devamını Okuyun</p>
+                    </NavDropdown.Item>
+                </NavDropdown>
+
+
+
+                <Nav.Link href="/contact" eventKey="link-4">İLETİŞİM</Nav.Link>
+
             </Nav>
         </>
     )
 }
+
+
 
 export default Menu;
