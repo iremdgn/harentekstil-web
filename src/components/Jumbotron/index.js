6973c0ee2b83ec4a5f30c0cae170234acb35f678
İrem Doğan <65050139+iremdgn@users.noreply.github.com>
2022-02-23 21:10:37 +0300

    update about page media

--- src/components/Jumbotron/index.js
+++ src/components/Jumbotron/index.js
@@ -5,17 +5,17 @@ import JumbotronText from './jumbotronText'
 function Jumbotron(props) {
     return (
         <>
-            <Row className="jumbotron mt-3">
-                <Col sm={5} >
+            <Row className="jumbotron mt-lg-3">
+                <Col xs={12} lg={5} >
                     <hr></hr>
-                    <div className="jumbotron-text py-2">
+                    <div className="jumbotron-text py-lg-2">
                         <JumbotronText
                             title={props.title}
                             text={props.text}
                         />
                     </div>
                 </Col>
-                <Col sm={7} style={{ backgroundImage: `url(${props.imageUrl})` }}>
+                <Col className="jumbotron-image" xs={12} lg={7} style={{ backgroundImage: `url(${props.imageUrl})` }}>
 
                 </Col>
             </Row>
