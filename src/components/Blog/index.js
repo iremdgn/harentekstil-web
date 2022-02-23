26fd6b50f8f58ab6574715907dd1652cc299adf8
İrem Doğan <65050139+iremdgn@users.noreply.github.com>
2022-02-23 21:27:02 +0300

    blog page update media

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -5,16 +5,16 @@ import Image from 'next/image'
 function Blog(props) {
     return (
         <>
-            <Container className="p-5 blog ">
-                <Row className={"p-4" + props.blogClass}>
-                    <Col>
+            <Container className="p-lg-5 p-4 my-3 my-lg-0 blog ">
+                <Row className={"p-lg-4" + props.blogClass}>
+                    <Col xs={12} lg={6}>
                         <Image src={props.imageUrl} alt="slider1" width="600" height="500"/>
                     </Col>
-                    <Col>
-                        <div className="ps-1 ms-4 blog-text">
+                    <Col xs={12} lg={6}>
+                        <div className="ps-1 ms-lg-4 blog-text">
                             <h1>{props.title}</h1>
-                            <p className="pt-3">{props.text}</p>
-                            <Button className={"py-2 mt-3 " + props.buttonClass}><a className="blog-button">Okumaya Devam Et</a></Button>
+                            <p className="pt-lg-3 pt-1">{props.text}</p>
+                            <Button className={"py-lg-2 mt-lg-3 " + props.buttonClass}><a className="blog-button">Okumaya Devam Et</a></Button>
                         </div>
                     </Col>
                 </Row>
