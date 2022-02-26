3c8b320538a4923905b9b5836d40ed9da950d738
İrem Doğan <65050139+iremdgn@users.noreply.github.com>
2022-02-26 18:42:25 +0300

    update get contact page data

--- src/components/Contact/contactInformations.js
+++ src/components/Contact/contactInformations.js
@@ -1,9 +1,29 @@
-import React from 'react'
-import { Form, Button } from 'react-bootstrap'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import {get} from '/src/services/request'
 
 
 function ContactInformation() {
+
+    const [contactData, setContactData] = useState(null);
+
+    const getContactData = async () => {
+        const response = await get('http://localhost:1337/api/contact-page');
+        console.log(response);
+        //console.log(response.data.data.attributes);
+        if (response.status === 200) {
+            setContactData(response.data.data.attributes);
+        }
+    }
+
+    useEffect(() => {
+        getContactData();
+    }, []);
+
     return (
+
+        contactData != null ? 
+
         <div className="contact ps-lg-5 ms-lg-5 pt-4 pt-lg-0">
             <div>
                 <h1 className="pb-lg-4">İletişim Bilgilerimiz</h1>
@@ -25,17 +45,17 @@ function ContactInformation() {
 
                 <div className="d-flex">
                     <p className="pe-2"> <span>Mail Adresi: </span></p>
-                    <p> <a href="mailto:info@harentekstil.com"> info@harentekstil.com</a> </p>
+                    <p> <a href="mailto:info@harentekstil.com">{contactData.eMail}</a> </p>
                 </div>
             </div>
 
             <div>
                 <h1 className="pb-2 pt-lg-3">Adresimiz</h1>
 
-                <p><span>Eskihisar mahahallesi 8015 sokak 1A/Blok No: 3 Merkezefendi/Denizli</span></p>
+                <p><span>{contactData.Address}</span></p>
             </div>
 
-        </div>
+        </div> : <p>Data Bulunamadı... :(</p>
     );
 }
 
---
+++ src/services/request/index.js
@@ -0,0 +1,60 @@
+import axios from 'axios';
+
+const config = {
+    headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer 1771aa6577113d6d37358a82653098b3711f476dc76dc5e5710368cd5599c3703499dbef1aa70f2ec39834635a0c0b2ab1980efe56a84226dff8f8f3d5c82597184977e359997789f45e6d1e39f5a489b3f1875c93cdea37c00040a385afe28478260b430c2327a22002c3164ad06baf2a9b8cab2563a377277da3d4e2264f6f'
+    }
+};
+
+export const post = (request, data, token, type) => {
+    return new Promise((resolve, reject) => {
+        axios
+            .post(request, data, config)
+            .then((response) => {
+                resolve(response);
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
+};
+
+export const deleteFunction = (request, data, token) => {
+    return new Promise((resolve, reject) => {
+        axios
+            .delete(request, config)
+            .then((response) => {
+                resolve(response);
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
+};
+
+export const put = (request, data, token, type) => {
+    return new Promise((resolve, reject) => {
+        axios
+            .put(request, data, config)
+            .then((response) => {
+                resolve(response);
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
+};
+
+export const get = (request, token) => {
+    return new Promise((resolve, reject) => {
+        axios
+            .get(request, config)
+            .then((response) => {
+                resolve(response);
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
+};
