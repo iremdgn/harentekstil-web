6a494f76f025510222592107f14091cc6f27d718
İrem Doğan <65050139+iremdgn@users.noreply.github.com>
2022-03-08 19:14:20 +0300

    update

--- src/components/BlogContent/blogContentBanner.js
+++ src/components/BlogContent/blogContentBanner.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 function BlogContentBanner(props) {
     return (
-        <div className="content-banner d-flex align-items-center" style={{ backgroundImage: `url(${props.imageUrl})` }}>
+        <div className="content-banner d-flex align-items-center mt-lg-3" style={{ backgroundImage: `url(${props.imageUrl})` }}>
             <hr className="me-3"></hr>
             <div className="text-area">
                 <h1>{props.title}</h1>
